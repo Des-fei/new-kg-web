f21fbac104f1e3d557ba6ec222d1f2d0785d6a3d
Des-fei <Destiny_1221@163.com>
2022-05-11 18:48:14 +0800

    X6

--- src/views/antvx6demo/demo.vue
+++ src/views/antvx6demo/demo.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="antv-x6">
-    <div class="left">
-      <div id="stencil" class="stencil"></div>
-      <div class="sign" v-if="flag" @click="hiddenStencil()">---</div>
-      <div class="sign" v-else @click="hiddenStencil()">+++</div>
-    </div>
+    <div id="stencil" class="stencil"></div>
     <div id="container" class="container"></div>
   </div>
 </template>
@@ -15,7 +11,7 @@ export default {
   name: "AntvX6",
   data() {
     return {
-      chartData: {
+      chartData1: {
         nodes: [
           {
             id: "node1", // String，可选，节点的唯一标识
@@ -41,9 +37,12 @@ export default {
           },
         ],
       },
+      chartData: {
+        nodes: [],
+        edges: [],
+      },
       graph: null,
       stencil: null,
-      flag: true,
     };
   },
   mounted() {
@@ -55,20 +54,46 @@ export default {
       background: {
         color: "#fffbe6", // 设置画布背景颜色
       },
+      // scroller: true, //滚动画布
+      // history: true, //撤消重做
+      clipboard: true, //剪切板
+      keyboard: true, //键盘快捷键
+      snapline: true, //对齐线
+      // mousewheel: {
+      //   enabled: true,
+      //   zoomAtMousePosition: true, //是否将鼠标位置作为中心缩放
+      //   modifiers: "ctrl", //设置修饰键
+      //   minScale: 0.5,
+      //   maxScale: 3,
+      // }, //鼠标滚轮缩放
+      selecting: {
+        enabled: true, //启用点选
+        rubberband: true, // 启用框选
+        showNodeSelectionBox: true, //是否显示节点的选择框
+      },
+      resizing: true, //缩放节点
+      // translating: true, //平移节点
+      // transforming:true,//平移和缩放节点的基础选项
       grid: {
         size: 10, // 网格大小 10px
         visible: true, // 渲染网格背景
       },
     });
-    graph.fromJSON(this.chartData);
+    graph.fromJSON(this.chartData1);
     // graph.zoom(-0.5);
     // graph.translate(80, 40);
 
     const stencil = new Addon.Stencil({
-      title: "基础组件",
-      target: graph,
-      stencilGraphWidth: 200,
-      stencilGraphHeight: 180,
+      title: "组件库",
+      target: graph, //目标画布
+      collapsable: true, // 分组是否可折叠，默认为 true
+      collapsed: true, // 初始状态是否为折叠状态，true为不折叠
+      stencilGraphWidth: 200, //模板画布宽度
+      stencilGraphHeight: 180, //模板画布高度
+      // graphPadding: 50, // 模板画布边距
+      search: true, //搜索选项
+      placeholder: "Search", //搜索文本框的 placeholder 文本
+      notFoundText: "No matches found", //未匹配到搜索结果时的提示文本
       groups: [
         {
           title: "A类组件",
@@ -84,17 +109,145 @@ export default {
         },
       ],
       layoutOptions: {
-        columns: 2,
-        columnWidth: 80,
-        rowHeight: 55,
+        columns: 2, //网格布局的列数，默认为 2。行数根据节点数自动计算
+        columnWidth: 80, //列宽
+        rowHeight: 55, //行高
+        // dx:10,//单元格在 X 轴的偏移量
+        // dy:10,//单元格在 Y 轴的偏移量
+        // marginX:0,//单元格在 X 轴的边距
+        // marginY:0,//单元格在 Y 轴的边距
+        center: true, //节点是否与网格居中对齐
+        resizeToFit: false, //是否自动调整节点的大小来适应网格大小
       },
     });
+    //挂载到页面
     document.getElementById("stencil").appendChild(stencil.container);
+
+    //自定义节点（组件）
+    Graph.registerNode(
+      "custom-rect",
+      {
+        inherit: "rect",
+        width: 66, // 默认宽度
+        height: 36, // 默认高度
+        attrs: {
+          body: {
+            rx: 10, // 圆角矩形
+            ry: 10,
+            strokeWidth: 1, //边框宽度
+            stroke: "#5F95FF", //边框颜色
+            fill: "#EFF4FF", //填充颜色
+          },
+          text: {
+            fontSize: 12,
+            fill: "#262626",
+            // refX: 10, // x 轴偏移，类似 css 中的 margin-left
+            // textAnchor: "left", // 左对齐
+          },
+        },
+      },
+      true
+    );
+    Graph.registerNode(
+      "custom-circle",
+      {
+        inherit: "circle",
+        width: 45, // 默认宽度
+        height: 45, // 默认高度
+        attrs: {
+          body: {
+            strokeWidth: 1, //边框宽度
+            stroke: "#5F95FF", //边框颜色
+            fill: "#EFF4FF", //填充颜色
+          },
+          text: {
+            fontSize: 12,
+            fill: "#262626",
+            // refX: 10, // x 轴偏移，类似 css 中的 margin-left
+            // textAnchor: "left", // 左对齐
+          },
+        },
+      },
+      true
+    );
+
+    //创建模板节点
+    const r1 = graph.createNode({
+      shape: "custom-rect",
+      label: "a1",
+    });
+    const r2 = graph.createNode({
+      shape: "custom-rect",
+      label: "a2",
+    });
+    const r3 = graph.createNode({
+      shape: "custom-circle",
+      label: "b1",
+    });
+    const r4 = graph.createNode({
+      shape: "custom-circle",
+      label: "b2",
+    });
+
+    //将模板节点添加到指定的群组中
+    stencil.load([r1, r2], "group1");
+    stencil.load([r3, r4], "group2");
+
+    this.graph = graph;
+    this.stencil = stencil;
+    this.getData();
   },
   methods: {
-    hiddenStencil() {
-      this.flag = !this.flag;
-      console.log(this.flag);
+    getData() {
+      let graph = this.graph;
+      let stencil = this.stencil;
+
+      //复制、粘贴和剪切
+      graph.bindKey(["meta+c", "ctrl+c"], () => {
+        const cells = graph.getSelectedCells();
+        if (cells.length) {
+          graph.copy(cells);
+        }
+        return false;
+      });
+      graph.bindKey(["meta+v", "ctrl+v"], () => {
+        if (!graph.isClipboardEmpty()) {
+          const cells = graph.paste({ offset: 32 });
+          graph.cleanSelection();
+          graph.select(cells);
+        }
+        return false;
+      });
+      graph.bindKey(["meta+x", "ctrl+x"], () => {
+        const cells = graph.getSelectedCells();
+        if (cells.length) {
+          graph.cut(cells);
+        }
+        return false;
+      });
+
+      //删除
+      graph.bindKey("backspace", () => {
+        const cells = graph.getSelectedCells();
+        if (cells.length) {
+          graph.removeCells(cells);
+        }
+      });
+
+      graph.on("node:mouseenter", ({ node }) => {
+        node.addTools({
+          name: "button-remove",
+          args: {
+            x: "100%",
+            y: 0,
+            offset: { x: -10, y: 10 },
+          },
+        });
+      });
+
+      graph.on("node:mouseleave", ({ node }) => {
+        node.removeTools();
+      });
     },
   },
 };
@@ -102,16 +255,11 @@ export default {
 
 <style lang="scss" scoped>
 .antv-x6 {
-  .left {
+  display: flex;
+  .stencil {
     width: 500px;
     height: 1080px;
-    position: absolute;
-    .sign {
-      background: red;
-    }
-  }
-  .container {
-    z-index: -1;
+    position: relative;
   }
 }
 </style>
